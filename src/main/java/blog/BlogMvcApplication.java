5fef80ab7cbdbca670d9c7203d7098e18ddd9e22
Petr Lorenc (PC) <lorenc55petr@seznam.cz>
2017-03-12 10:24:07 +0100

    init

--- src/main/java/blog/BlogMvcApplication.java
+++ src/main/java/blog/BlogMvcApplication.java
@@ -1,12 +1,16 @@
 package blog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 /**
  * Created by United121 on 1/23/2017.
  */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class BlogMvcApplication {
     public static void main(String[] args) {
         //Calling SpringApplication.run(…) will start an embedded Tomcat Web application server at http://localhost:8080
